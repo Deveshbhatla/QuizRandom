50d503c02a012166c0b9da0e89de7384c1a95118
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2020-06-10 19:14:40 +0530

    Create ChemistryActivity.java

---
+++ app/src/main/java/com/devesh/devesh_quiz/Chemistry/ChemistryActivity.java
@@ -0,0 +1,41 @@
+package com.devesh.devesh_quiz.Chemistry;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.devesh.devesh_quiz.MainActivity;
+import com.devesh.devesh_quiz.R;
+
+
+public class ChemistryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chemistry);
+        Button startbutton=(Button)findViewById(R.id.button);
+        final EditText nametext=(EditText)findViewById(R.id.editName);
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=nametext.getText().toString();
+                Intent intent=new Intent(getApplicationContext(), ChemistryQuestionsActivity.class);
+                intent.putExtra("myname",name);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
