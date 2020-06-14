d43635598a63b0603c9aba4ec7e648a0ef979bf7
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2020-06-14 21:46:35 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/devesh/devesh_quiz/MainActivity.java
@@ -0,0 +1,54 @@
+package com.devesh.devesh_quiz;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PlayFragment()).commit();
+        }
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_play:
+                            selectedFragment = new PlayFragment();
+                            break;
+                        case R.id.nav_more:
+                            selectedFragment = new MoreFragment();
+                            break;
+
+
+                    }
+
+                    assert selectedFragment != null;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
