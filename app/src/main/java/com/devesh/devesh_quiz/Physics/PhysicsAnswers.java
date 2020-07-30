5b0bfc2395bf4ebfb0eda19231e940ef8c44109d
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2020-07-30 22:19:45 +0530

    Create PhysicsAnswers.java

---
+++ app/src/main/java/com/devesh/devesh_quiz/Physics/PhysicsAnswers.java
@@ -0,0 +1,16 @@
+package com.devesh.devesh_quiz.Physics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.devesh.devesh_quiz.R;
+
+public class PhysicsAnswers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physics_answers);
+    }
+}
